d310ae81eba8edbf688db45dacb8c17329415ccc
zantaxpro <zantaxpro@gmail.com>
2025-06-14 09:26:26 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -38,7 +38,7 @@ async (conn, mek, m, { from, quoted, pushname, reply }) => {
 `;
 
         const sentMsg = await conn.sendMessage(from, {
-           imageUrl = 'https://i.ibb.co/GvsM5CsT/SulaMd.jpg',
+            image: { url: imageUrl },
             caption: selectionMessage,
             contextInfo: { forwardingScore: 999, isForwarded: true },
         }, { quoted: mek });
