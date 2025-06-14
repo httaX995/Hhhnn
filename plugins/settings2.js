2d2d8ca29305de1e9f5314cf7c25a226da464b22
zantaxpro <zantaxpro@gmail.com>
2025-06-14 12:13:18 +0530

    Create settings2.js

---
+++ plugins/settings2.js
@@ -0,0 +1,11 @@
+{
+  "LANG": "EN",
+  "ANTI_BAD": [],
+  "MAX_SIZE": 100,
+  "ONLY_GROUP": false,
+  "ANTI_LINK": [],
+  "ANTI_BOT": [],
+  "ALIVE": "default",
+  "FOOTER": "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+  "LOGO": "https://i.ibb.co/5g5frmz9/thisjpg.jpg"
+}
