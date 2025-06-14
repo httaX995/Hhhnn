38de23d0a6e221141d73e8c25220eded3d027560
zantaxpro <zantaxpro@gmail.com>
2025-06-14 23:29:20 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -26,24 +26,37 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
 `;
 
-        // Send the status message with an image
-        await conn.sendMessage(from, { 
-            image: { url: `https://files.catbox.moe/68rzgj.jpg` },  // Image URL
-            caption: status,
-            contextInfo: {
-                mentionedJid: [m.sender],
-                forwardingScore: 999,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363401249335754@newsletter',
-                    newsletterName: 'MD',
-                    serverMessageId: 143
-                }
+await conn.sendMessage(
+    m.chat,
+    {
+        document: fs.readFileSync("./package.json"),
+        fileName: "Qᴜᴇᴇɴ ᴇʟꜱᴀ𝗫 ᴍᴅ ",
+        mimetype: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        fileLength: 99999999999999,
+        pageCount: 2024,
+        caption: menumsg,
+        contextInfo: {
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterName: 'Qᴜᴇᴇɴ ᴇʟꜱᴀ𝗫 ᴍᴅ ',
+                newsletterJid: "120363373642098017@newsletter"
+            },
+            externalAdReply: {
+                title: "> Qᴜᴇᴇɴ ᴇʟꜱᴀ𝗫 ᴍᴅ ",
+                body: '',
+                thumbnailUrl: 'https://files.catbox.moe/xnot7v.jpg',
+                sourceUrl: "https://github.com/niko-boy3/ElsaX_MD",
+                mediaType: 1,
+                renderLargerThumbnail: true
             }
-        }, { quoted: mek });
+        }
+    },
+    { quoted: mek }
+);
 
-    } catch (e) {
-        console.error("Error in alive command:", e);
-        reply(`An error occurred: ${e.message}`);
-    }
-});
+} catch(e){
+    console.log(e)
+    reply(`${e}`)
+}
+})
