1f58a466c1576629ac119b3ac127055fd196c320
zantaxpro <zantaxpro@gmail.com>
2025-06-15 10:46:27 +0530

    Create dog.js

---
+++ plugins/dog.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "dog",
+    desc: "Fetch a random dog image.",
+    category: "fun",
+    react: "🐶",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://dog.ceo/api/breeds/image/random`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '> 🧙‍♂️ 𝐙𝐀𝐍𝐓𝐀 × 𝐌𝐃 𝐎𝐅𝐂 🧙‍♂️ ' }, { quoted: mek });
+    } catch (e) {
+        console.log(e); // *➥𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐲 - : 𝐌𝐑 𝐒𝐔𝐑𝐀𝐍𝐆𝐀 𝐎𝐅𝐂 🗿*
+        reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
+    }
+});
