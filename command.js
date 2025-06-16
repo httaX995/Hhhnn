a85103b51b572d7219a629514248812a13ceaf7a
zantaxpro <zantaxpro@gmail.com>
2025-06-16 15:07:56 +0530

    Update command.js

--- command.js
+++ command.js
@@ -1,22 +1,37 @@
-{
-    "shutdown,broadcast,block,unblock,clearchats,jid,gjid,restart,update": "owner",
-    "tts,toptt,sticker,emojimix,trt": "convert",
-    "gemini,ai,aiimage,meta,totext": "ai",
-    "esananews,img,bingimg,movie,news,pinterest,wallpaper": "search",
-    "tiktok,fb,twitter,mediafire,ig,baiscope,ginisisila,apk,gdrive,spotify,tnw,tww,ta,fbhd,fba": "download",
-    "animegirl,quote,joke,hack,fact,dog": "fun",
-    "alive,system,runtime,ping,owner": "main",
-    "write,starsnight,leaves,metalstar,starzodiac,avatargold,frozen,neondevil,snow3d,birthday,colorfulangel,makingneon,beautifulgold,thunder,galaxy1,advancedglow,typography,pixelglitch,neonglitch,glitch,flag,flag3,deleting,blackpink,glowing,underwater,logomaker,cartoon,papercut,watercolor,effectcloud,gradien,summerbeach,luxurygold,multicolourneon,sandsummer,galaxywallpaper,1971,royal,freecreate,galaxystyle,lighteffect": "logo",
-    "kick,add,delete,kickall,opentime,closetime,tagall,tagadmin,mute,unmute,promote,demote,setname,setdesc,invite,join,leave": "group",
-    "gpass,githubstalk,srepo,weather,cal,ss,currency,translate,tempmail,checkmail,delmail,password,hijact,fancy,removebg,boom,enhance,toimg,getdetails,tourl": "usefull",
-    "setprefix,setnews,setreact,btnmsg,autoreadstatus,resetmovie,setmode,setbotnumber,autovoice,autosticker,autoreply,autoreact,welcome,antibad,antibot,antilink,anticall,antidelete,allwaysonline,moroccoblock,readcmd,autotyping,autorecording,autonews,ownerreact,aimode,setmovie,ban,unban,addsudo,delsudo,setanime,resetanime": "settings",
-    "xnxx,xvideos,phub": "nsfw",
-    "sinhalasub,cinesubz,ytsmx,1337x": "movie",
-    "bully,cuddle,cry,hug,awoo,kiss,lick,pat,smug,bonk,yeet,blush,smile,wave,highfive,handhold,nom,bite,glomp,slap,kill,kick,happy,wink,poke,dance,cringe": "reaction",
-    "hwaifu,hneko,trap,blowjob": "hentai",
-    "menu,list":"menu",
-    "pair,forward,vv":"functions",
-    "song,video,yt1s,yt2s,yt3s,ytmp4,ytmp3,yts": "youtube",
-    "bug,killno,killhim,killnow": "bug2"
-}
-  
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                   𝗟𝗢𝗗-𝗧𝗘𝗠-𝗠𝗗                                               //
+//                                                                                                      //
+//                                         Ｖ：4.0                                                       //
+//
+//
+//░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░██╗░░░░░██╗██╗░░░██╗  ██╗░░██╗██████╗░██████╗░░█████╗░
+//██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░██║░░░░░██║██║░░░██║  ╚██╗██╔╝██╔══██╗██╔══██╗██╔══██╗
+//██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔██╗██║░░░░░██║██║░░░██║  ░╚███╔╝░██████╔╝██████╔╝██║░░██║
+//╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚████║██╗░░██║██║░░░██║  ░██╔██╗░██╔═══╝░██╔══██╗██║░░██║
+//░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚███║╚█████╔╝╚██████╔╝  ██╔╝╚██╗██║░░░░░██║░░██║╚█████╔╝
+//░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░░╚═════╝░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░                                           
+//
+//
+//
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : © 𝗟𝗢𝗗-𝗧𝗘𝗠-𝗠𝗗
+//  * @version      : 1.0
+//  * @author       : 𝗖𝗬𝗕𝗘𝗥 𝗗𝗜𝗡𝗨 𝗜𝗗
+//  * @youtube      : https://www.youtube.com/@..
+//  * @description  : © 𝙻𝙾𝙳-𝚃𝙴𝙼-𝙼𝙳, A Multi-functional WhatsApp bot created by 𝙲𝚈𝙱𝙴𝚁 𝙳𝙸𝙽𝚄 𝙸𝙳.
+//*
+//*
+//Base by 𝙲𝚈𝙱𝙴𝚁 𝙳𝙸𝙽𝚄 𝙸𝙳
+//GitHub: @xxxmdowner
+//WhatsApp: 94764972683
+//Want more free bot scripts? Subscribe to my YouTube channel: https://youtube.com/@...
+//   * Created By GitHub: CYBER DINU ID 
+//   * Credit To CYBER DINU ID 
+//   * © 2025 © ʟᴏᴅ-ᴛᴇᴍ-ᴍᴅ.
+// ⛥┌┤
+// */
+
+function _0x5e3c(_0x57ce6e,_0x53ffaf){var _0x1c2707=_0x1c27();return _0x5e3c=function(_0x5e3c38,_0xd6bd12){_0x5e3c38=_0x5e3c38-0xbf;var _0x3dd15a=_0x1c2707[_0x5e3c38];return _0x3dd15a;},_0x5e3c(_0x57ce6e,_0x53ffaf);}(function(_0x144065,_0xa2c29e){var _0x560585=_0x5e3c,_0x29208a=_0x144065();while(!![]){try{var _0x4a0e2c=parseInt(_0x560585(0xbf))/0x1+parseInt(_0x560585(0xc9))/0x2*(-parseInt(_0x560585(0xcc))/0x3)+parseInt(_0x560585(0xd0))/0x4+parseInt(_0x560585(0xd2))/0x5+-parseInt(_0x560585(0xcd))/0x6*(parseInt(_0x560585(0xc7))/0x7)+-parseInt(_0x560585(0xc3))/0x8*(-parseInt(_0x560585(0xc8))/0x9)+parseInt(_0x560585(0xcb))/0xa*(-parseInt(_0x560585(0xc0))/0xb);if(_0x4a0e2c===_0xa2c29e)break;else _0x29208a['push'](_0x29208a['shift']());}catch(_0x4a5fc8){_0x29208a['push'](_0x29208a['shift']());}}}(_0x1c27,0x1dc28));var commands=[];function cmd(_0x1c0d65,_0x3640a2){var _0x193a77=_0x5e3c,_0x59d3eb=_0x1c0d65;_0x59d3eb[_0x193a77(0xc6)]=_0x3640a2;if(!_0x59d3eb['dontAddCommandList'])_0x59d3eb[_0x193a77(0xc5)]=![];if(!_0x1c0d65[_0x193a77(0xd1)])_0x1c0d65[_0x193a77(0xd1)]='';if(!_0x59d3eb[_0x193a77(0xcf)])_0x59d3eb['fromMe']=![];if(!_0x1c0d65[_0x193a77(0xca)])_0x59d3eb[_0x193a77(0xca)]=_0x193a77(0xce);if(!_0x1c0d65['filename'])_0x59d3eb[_0x193a77(0xc2)]=_0x193a77(0xc4);return commands[_0x193a77(0xc1)](_0x59d3eb),_0x59d3eb;}module['exports']={'cmd':cmd,'AddCommand':cmd,'Function':cmd,'Module':cmd,'commands':commands};function _0x1c27(){var _0x49da3c=['misc','fromMe','422268irVFBH','desc','950330cGefos','138148WrziTL','22fEjBQO','push','filename','48DKMciK','Not\x20Provided','dontAddCommandList','function','247037hybpzZ','46836OWKRnH','20yRzmIb','category','764630vMyNmY','25293oXJNzk','18zaRLTK'];_0x1c27=function(){return _0x49da3c;};return _0x1c27();}
+
